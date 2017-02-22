f941843b741d05f4ab1040167612d2fd5c68869a
skomer <parusmontanus@gmail.com>
2017-02-22 14:40:42 +0000

    Creating GameLogicTest class

---
+++ app/src/test/java/com/elderj/pointsiest/GameLogicTest.java
@@ -0,0 +1,30 @@
+package com.elderj.pointsiest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class GameLogicTest {
+
+    Sportsperson moFarah = new Sportsperson(
+            "Mo",
+            "Farah",
+            "Mo Farah",
+            99,
+            999.999,
+            "mo farah profile pic url"
+    );
+
+    Sportsperson peytonManning = new Sportsperson(
+            "Peyton",
+            "Manning",
+            "Peyton Manning",
+            3,
+            311.113,
+            "peyton manning profile pic url"
+    );
+
+
+    
+
+}
