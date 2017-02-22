27029bbd542492f4b90a0cea83b3bd5895087002
skomer <parusmontanus@gmail.com>
2017-02-22 15:07:37 +0000

    Writing @Before method in GameLogicTest

--- app/src/test/java/com/elderj/pointsiest/GameLogicTest.java
+++ app/src/test/java/com/elderj/pointsiest/GameLogicTest.java
@@ -1,30 +1,46 @@
 package com.elderj.pointsiest;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 
 public class GameLogicTest {
 
-    Sportsperson moFarah = new Sportsperson(
-            "Mo",
-            "Farah",
-            "Mo Farah",
-            99,
-            999.999,
-            "mo farah profile pic url"
-    );
+    @Before
+    public void before() {
+        Sportsperson moFarah = new Sportsperson(
+                "Mo",
+                "Farah",
+                "Mo Farah",
+                99,
+                999.999,
+                "mo farah profile pic url"
+        );
 
-    Sportsperson peytonManning = new Sportsperson(
-            "Peyton",
-            "Manning",
-            "Peyton Manning",
-            3,
-            311.113,
-            "peyton manning profile pic url"
-    );
+        Sportsperson peytonManning = new Sportsperson(
+                "Peyton",
+                "Manning",
+                "Peyton Manning",
+                3,
+                311.113,
+                "peyton manning profile pic url"
+        );
+
+        ArrayList<Sportsperson> sportspeople = new ArrayList<>();
+        sportspeople.add(moFarah);
+        sportspeople.add(peytonManning);
+    }
+
+    @Test
+    public void gameCanTellWhichSportspersonHasMostPoints() {
 
 
-    
+
+    }
+
 
 }
